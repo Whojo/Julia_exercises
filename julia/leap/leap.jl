c3a920fcb6d6fd6004904a1ee5abfdd679cf3caf
Guillaume THOMAS <guillaume1.thomas@epita.fr>
2022-05-25 19:02:38 +0200

    [UP] leap: factorise

--- julia/leap/leap.jl
+++ julia/leap/leap.jl
@@ -5,5 +5,6 @@ Return `true` if `year` is a leap year in the gregorian calendar.
 
 """
 function is_leap_year(year)
-    return mod(year, 4) == 0 && (mod(year, 100) != 0 || mod(year, 400) == 0)
+    divisible_by(n) = year % n == 0
+    return divisible_by(4) && !divisible_by(100) || divisible_by(400)
 end
