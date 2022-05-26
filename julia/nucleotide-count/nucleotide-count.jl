e2f7cf3473c4f2cd3635e2d783963976b875ed6a
Guillaume THOMAS <guillaume1.thomas@epita.fr>
2022-05-26 12:36:50 +0200

    [UP] Nucleotide: use the dot operator

--- julia/nucleotide-count/nucleotide-count.jl
+++ julia/nucleotide-count/nucleotide-count.jl
@@ -7,10 +7,11 @@ Invalid strands raise a `DomainError`.
 
 """
 function count_nucleotides(strand)
-    nucleotides_counter = Dict('A' => 0, 'C' => 0, 'G' => 0, 'T' => 0)
+    bases = ('A', 'C', 'G', 'T')
+    nucleotides_counter = Dict(bases .=> 0)
     for nucleotide in strand
         if (nucleotide ∉ keys(nucleotides_counter))
-            throw(DomainError(nucleotide, "Passed letter is not a nucleotide ('A', 'C', 'G' or 'T')"))
+            throw(DomainError(nucleotide, "Passed letter is not a nucleotide $bases"))
         end
 
         nucleotides_counter[nucleotide] += 1
