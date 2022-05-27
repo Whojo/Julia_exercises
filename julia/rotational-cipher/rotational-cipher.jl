d7e63b156ef87692d0eb9700c2918873665a3b0e
Guillaume THOMAS <guillaume1.thomas@epita.fr>
2022-05-27 20:09:40 +0200

    [UP] Rotational Cipher: remove global variable

--- julia/rotational-cipher/rotational-cipher.jl
+++ julia/rotational-cipher/rotational-cipher.jl
@@ -5,9 +5,9 @@ return the encoded `text` through the rotation cipher
 """
 # Improvements
 # - Precompute a translation dictionnary for larger strings
-NB_LETTERS = 26
 
 function rotate(key, clear_char::Char)
+    NB_LETTERS = 26
     isletter(clear_char) || return clear_char
 
     first = isuppercase(clear_char) ? 'A' : 'a'
