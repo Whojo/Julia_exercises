76b3e2a28bf7f3338a37d68bec26cf55b43decf0
Guillaume THOMAS <guillaume1.thomas@epita.fr>
2022-05-28 11:09:23 +0200

    [UP] Rotational Cipher: cache for longer string

--- julia/rotational-cipher/rotational-cipher.jl
+++ julia/rotational-cipher/rotational-cipher.jl
@@ -6,6 +6,20 @@ return the encoded `text` through the rotation cipher
 # Improvements
 # - Precompute a translation dictionnary for larger strings
 
+function translate(char, key, first)
+   (char - first + key) % 26 + first
+end
+
+function rotate_long(key, clear_text::String)
+    translate = Dict(c => translate(c, key, first(range))
+                     for range in ('a':'z', 'A':'Z')
+                     for c in range)
+
+    map(clear_text) do char
+        haskey(translate, char) ? translate[char] : char
+    end
+end
+
 function rotate(key, clear_char::Char)
     NB_LETTERS = 26
     isletter(clear_char) || return clear_char
