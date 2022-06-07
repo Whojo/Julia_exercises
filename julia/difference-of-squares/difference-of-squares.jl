842fd7ea5c3132fd88df03059a241f4425a7801e
Guillaume THOMAS <guillaume1.thomas@epita.fr>
2022-06-07 16:27:16 +0200

    [UP] Difference of squares: simple use of implicit multiplication

--- julia/difference-of-squares/difference-of-squares.jl
+++ julia/difference-of-squares/difference-of-squares.jl
@@ -5,7 +5,7 @@ end
 
 "Sum the squares of the first `n` positive integers"
 function sum_of_squares(n)
-    n * (n + 1) * (2 * n + 1) ÷ 6
+    n * (n + 1) * (2n + 1) ÷ 6
 end
 
 "Subtract the sum of squares from square of the sum of the first `n` positive ints"
