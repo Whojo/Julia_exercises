c60087f9b65f25e5bf895a1aa62173ac522dd672
Guillaume THOMAS <guillaume1.thomas@epita.fr>
2022-06-19 22:40:42 +0200

    [ADD] MyMiniPackage: Test with identity kernel

--- MyMiniPackage/test/runtests.jl
+++ MyMiniPackage/test/runtests.jl
@@ -9,4 +9,13 @@ using Test
         result = my_convolution(image, kernel)
         @test size(result) == size(image)
     end
+    
+    @testset "Identity Kernel" begin
+        image = zeros(Int8, 32, 32)
+        kernel = zeros(Int8, 3, 3)
+        kernel[2, 2] = 1
+
+        result = my_convolution(image, kernel)
+        @test result == image
+    end
 end
