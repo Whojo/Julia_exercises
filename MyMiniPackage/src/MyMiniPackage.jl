91e417f5c6d15233ca4217704545c2196992db33
Guillaume THOMAS <guillaume1.thomas@epita.fr>
2022-06-23 22:23:37 +0200

    [UP] MyMiniPackage: use more cleaver for loop

--- MyMiniPackage/src/MyMiniPackage.jl
+++ MyMiniPackage/src/MyMiniPackage.jl
@@ -17,18 +17,12 @@ function my_sequential_convolution(image::Matrix, kernel::Matrix)
     mid_ker = x_ker_dim ÷ 2
 
     res = zeros(eltype(image), size(image))
-    for x in 1:x_img_dim
-        for y in 1:y_img_dim
-            for x_ker in 1:x_ker_dim
-                for y_ker in 1:y_ker_dim
-                    new_x, new_y = get_relative_indexing(mid_ker, x, y, x_ker, y_ker)
-                    if (new_x < 1 || new_y < 1 || new_x > x_img_dim || new_y > y_img_dim)
-                        continue
-                    end
-                    res[x, y] += image[new_x, new_y] * kernel[x_ker, y_ker]
-                end
-            end
+    for x = 1:x_img_dim, y = 1:y_img_dim, x_ker = 1:x_ker_dim, y_ker = 1:y_ker_dim
+        new_x, new_y = get_relative_indexing(mid_ker, x, y, x_ker, y_ker)
+        if (new_x < 1 || new_y < 1 || new_x > x_img_dim || new_y > y_img_dim)
+            continue
         end
+        res[x, y] += image[new_x, new_y] * kernel[x_ker, y_ker]
     end
 
     res
